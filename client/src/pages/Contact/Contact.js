c75ea12888172bc1fceee038cbfa487d568d28f0
aassadi <33338308+aassadi@users.noreply.github.com>
2018-07-17 18:32:06 -0400

    add-jason-file

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -10,7 +10,29 @@ class Contact extends Component {
             <div>
             <Navbar />
           <Container>
-         <div>contact</div>
+          <div class="">Address:</div>
+          <div>4395 SE 95th St, Ocala, FL 34480</div>
+          <br/>
+          <div class="">Contact:</div>
+          <div>352-307-1071</div>
+          <div>autowerx1@yahoo.com</div>
+          <br/>
+          <div class="">Hours:</div>
+          <div>
+            Monday:    9AM-5PM
+            <br/>
+            Tuesday:   9AM-5PM
+            <br/>
+            Wednesday: 9AM-5PM
+            <br/>
+            Thursday:  9AM-5PM
+            <br/>
+            Friday:    9AM-5PM
+            <br/>
+            Saturday:  9AM-4PM
+            <br/>
+            Sunday:    Closed
+          </div>
           </Container>
           </div>
         )
