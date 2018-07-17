6c24583c0e6e2e019a8a66437fca5a00abbf7c5e
Ghusoon <ga.ghusoon@gmail.com>
2018-07-17 19:24:41 -0400

    test

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -10,12 +10,14 @@ class About extends Component {
             <div>
             <Navbar />
           <Container>
-            <div>We are pleased to service retail customers, insurance companies, body shops, garages, and dealerships and are able 
+            <div className="aboutP">
+            <p>We are pleased to service retail customers, insurance companies, body shops, garages, and dealerships and are able 
               to locate even the hardest to find parts available for our customers.  All of our parts are “clean recycled” OEM parts 
               acquired from insurance automobile auctions.  In order to strive to provide the best customer service possible, we are 
               happy to offer video of most engines running and tested, extended warranties on our parts, daily delivery and shipping 
               within the state of Florida, and as always, nationwide shipping to our out-of-state customers.
-            </div>
+           </p>
+              </div>
           </Container>
           </div>
         )
